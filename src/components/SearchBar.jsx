fa1573836759ff8b51a308afb92a58a3c02f4282
Liliana Leiva <liliana-carmen@hotmail.com>
2022-04-12 13:21:58 -0300

    css modificaciones

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,32 +9,21 @@ export default function SearchBar({onSearch}) {
         setCity(e.target.value);
     }
     return (
-        <div class="input-group">
-            <form onSubmit={(e) => {
-                e.preventDefault();
+        <form onSubmit={(e) => {
+            e.preventDefault();
                 onSearch(city);
             }}>
+                <div class="input-group">
                 <input 
                     type="text"
                     placeholder="City..."
                     className="form-control"
-                    // aria-label="Recipient's username"
-                    // aria-describedby="button-addon2"
-                    //value={city}
+                    value={city}
                     onChange={handleInputChange}                  
                 />
 
-                {/* <input className="btn btn-warning" onChange={onSearch} type="submit" value="Agregar" /> */}
-
-                <button
-                    type="submit"
-                    class="btn btn-outline-secondary"
-                    id="button-addon2"
-                    onChange={onSearch}
-                >
-                    Add
-                </button>
-            </form>
-        </div>
+                <input type="submit" className="btn btn-outline-secondary" onChange={onSearch} value="Agregar" />
+            </div>
+        </form>
     );
 }
