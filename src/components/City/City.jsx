43528167cf31941fcb615127d6152ff904f946c1
Liliana Leiva <liliana-carmen@hotmail.com>
2022-06-01 18:10:26 -0300

    arreglo bug citydetails y ellipsis en title

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -1,56 +1,96 @@
 import "./City.css"
-import { useState, useParams } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 
-export default function City({city}) {
+export default function City({ city }) {
 
-  // const { id } = useParams();
+  const { id } = useParams();
 
-  // const [infocity, setInfocity] = useState({});
-  
-  // const apiKey = process.env.REACT_APP_API_KEY;
-  // fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}&units=metric`)
-  //       .then(response => response.json())
-  //       .then((props) => {
-  //         const city = {
-  //           min: Math.round(props.main.temp_min),
-  //           max: Math.round(props.main.temp_max),
-  //           img: props.weather[0].icon,
-  //           id: props.id,
-  //           wind: props.wind.speed,
-  //           temp: props.main.temp,
-  //           name: props.name,
-  //           weather: props.weather[0].main,
-  //           clouds: props.clouds.all,
-  //           latitud: props.coord.lat,
-  //           longitud: props.coord.lon
-  //         }
-  //         setInfocity(infocity => [...infocity, city])
-          
-  //       });
+  const [infoCity, setInfoCity] = useState({});
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then((props) => {
+      const city = {
+        min: Math.round(props.main.temp_min),
+        max: Math.round(props.main.temp_max),
+        img: props.weather[0].icon,
+        id: props.id,
+        wind: props.wind.speed,
+        temp: props.main.temp,
+        name: props.name,
+        weather: props.weather[0].main,
+        clouds: props.clouds.all,
+        latitud: props.coord.lat,
+        longitud: props.coord.lon
+      }
+      setInfoCity(city)
+    });
 
 
   return (
-      <div className="card text-dark bg-light mb-3" >
-        <div className="card-header">
-            <h4 className="card-title">{city.name} </h4>
-            <Link to="/">
-              <button className="btn btn-danger">↩</button>
-            </Link>
-        </div>
-        <div class="card-body">
-            <div className="temp">
-                <p className="card-text">Min temp: {city.min}º</p>
-                <p className="card-text">Max temp: {city.max}º</p>
-                <p className="card-text">Wind: {city.wind} m/s</p>
-                <p className="card-text">Clouds: {city.clouds} %</p>
-                <p className="card-text">Weather: {city.weather}</p>
-                <p className="card-text">Latitud: {city.latitud}º</p>
-                <p className="card-text">Longitud: {city.longitud}º</p>
-            </div>
+    <div className="card text-dark bg-light mb-3" >
+      <div className="card-header">
+        <h4 className="card-title">{infoCity.name} </h4>
+        <Link to="/">
+          <button className="btn btn-danger">↩</button>
+        </Link>
+      </div>
+      <div class="card-body">
+
+
+        <div className="temp">
+          {
+            (!infoCity.name && !infoCity.min && !infoCity.max && !infoCity.wind && !infoCity.clouds && !infoCity.weather && !infoCity.longitud && !infoCity.latitud && !infoCity.img)
+              ? <p className="card-text">Cargando...</p>
+              : null
+          }
+          {
+            infoCity.min
+              ? <p className="card-text">Min temp: {infoCity.min}º</p>
+              : null
+          }
+          {
+            infoCity.max
+              ? <p className="card-text">Max temp: {infoCity.max}º</p>
+              : null
+          }
+          {
+            infoCity.wind
+              ? <p className="card-text">Wind: {infoCity.wind} m/s</p>
+              : null
+          }
+          {
+            infoCity.clouds
+              ? <p className="card-text">Clouds: {infoCity.clouds} %</p>
+              : null
+          }
+          {
+            infoCity.weather
+              ? <p className="card-text">Weather: {infoCity.weather}</p>
+              : null
+          }
+          {
+            infoCity.latitud
+              ? <p className="card-text">Latitud: {infoCity.latitud}º</p>
+              : null
+          }
+          {
+            infoCity.longitud
+              ? <p className="card-text">Longitud: {infoCity.longitud}º</p>
+              : null
+          }
+          {
+            infoCity.img
+              ? <img className="city-detail" alt='weather img' src={"http://openweathermap.org/img/wn/" + infoCity.img + "@2x.png"} />
+              : null
+          }
         </div>
       </div>
+    </div>
   );
 }
 
